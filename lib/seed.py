097ccf5c2b206f4600cacad1263efb7b7caac8ec
Kanny <kanny.ghafour87@gmail.com>
2023-12-18 22:12:18 -0600

    adding seed data

--- lib/seed.py
+++ lib/seed.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-# lib/debug.py
-
 
 from models.__init__ import CONN, CURSOR
 from models.parents import Parent
 from models.children import Children
-import ipdb
 
-def reset_database():
+def seed_database():
     Children.drop_table()
     Parent.drop_table()
     Parent.create_table()
     Children.create_table()
 
+    # Create seed data
     shereen = Parent.create("Ali", "25")
     kanny = Parent.create(
         "Human Resources", "Building C, East Wing")
@@ -20,7 +18,7 @@ def reset_database():
     Children.create("Shayar", "girl", shereen.id)
     Children.create("Yasir", "girl", kanny.id)
     Children.create("Deya", "boy", kanny.id)
- 
-reset_database()
-ipdb.set_trace()
 
+
+seed_database()
+print("Seeded database")
